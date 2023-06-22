2fd93ddb263bb313c242627bf3c418c8cc6d6cf2
Fenrir-23 <germangeneral2301@gmail.com>
2023-06-22 21:10:03 +0200

    Resource v1

---
+++ TextBased_RTS/Business Layer/Resource.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBased_RTS
+{
+    internal class Resource
+    {
+        private string typeName;
+        private int income = 0; //Zero income by default
+
+        public Resource(string type)
+        {
+            setType(type);
+
+        }
+
+        private void setType(string myType)
+        {
+            try
+            {
+                switch (myType)
+                {
+                    case "Oxygen":
+                        typeName = "Oxygen";
+                        break;
+                    case "Water":
+                        typeName = "Water";
+                        break;
+                    case "Metal":
+                        typeName = "Metal";
+                        break;
+                    case "Helium3":
+                        typeName = "Helium3";
+                        break;
+                    default:
+                        throw new Exception("Invalid Resource Type");
+                }
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public string getType() { return typeName; }
+
+        public void addIncome(int num)
+        {
+            income += num;
+        }
+
+        public int getIncome() { return income;}
+
+    }
+}
