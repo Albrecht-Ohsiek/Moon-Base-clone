da577c465406aa26377212052ebec8cb420a3cad
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-08-09 16:12:48 +0200

    ReCreated Project in new IDE - Essentials only

---
+++ Text-Based-RTS-Client/Console-Interface/Client-Display-Console.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Text_Based_RTS_Client
+{
+    public class Client_Display_Console
+    {
+        public void myTest(){
+            Console.WriteLine("Hello Class in new folder");
+        }
+    }
+}
---
+++ Text-Based-RTS-Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Text_Based_RTS_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Client_Display_Console myTest= new Client_Display_Console();
+            myTest.myTest();
+
+        }
+    }
+}
+
+
